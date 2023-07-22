f32ba43e6b3cd5fd44c60b628e18acd38714362d
Nachiappan <nachiappann@microsoft.com>
2023-07-22 13:45:33 +0530

    TypeScript app

--- my-typescript-app/src/App.js
+++ my-typescript-app/src/App.js
@@ -1,20 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import {Bitbucket} from 'bitbucket';
-
 
 function App() {
-
-
-const clientOptions = {
-  baseUrl: 'https://api.bitbucket.org/2.0',
-  request: {
-    timeout: 10,
-  },
-}
-
-const bitbucket = new Bitbucket(clientOptions);
-
   return (
     <div className="App">
       <header className="App-header">
