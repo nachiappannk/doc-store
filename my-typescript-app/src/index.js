bdcf1d8245825cbe86ca088e7647e1ca9f833511
Nachiappan <nachiappann@microsoft.com>
2023-07-23 10:33:24 +0530

    Cleaned up the mess

--- my-typescript-app/src/index.js
+++ my-typescript-app/src/index.js
@@ -15,65 +15,42 @@ const authConfig = {
   redirectUri: 'http://localhost:3000/',
   scope: 'api read_api read_user create_runner read_repository write_repository read_registry write_registry read_observability write_observability sudo admin_mode openid profile email',
   onRefreshTokenExpire: (event) => window.confirm('Session expired. Refresh page to continue using the site?') && event.login(),
-  autoLogin : false
+  autoLogin : false,
+  decodeToken : false
 }
 
 const UserInfo = () => {
-  const {token, tokenData, login, logOut} = useContext(AuthContext);
+  const {token, login, logOut} = useContext(AuthContext);
   const authContext = useContext(AuthContext);
 
   console.log(authContext);
   console.log(token);
-  console.log(tokenData);
   return <>
-
-<>
-      {token ? (
-        <>
-          <div>
-            <h4>Access Token (JWT)</h4>
-            <pre
-              style={{
-                width: '400px',
-                margin: '10px',
-                padding: '5px',
-                border: 'black 2px solid',
-                wordBreak: 'break-all',
-                whiteSpace: 'break-spaces',
-              }}
-            >
-              {token}
-            </pre>
-          </div>
-          <div>
-            <h4>Login Information from Access Token (Base64 decoded JWT)</h4>
-            <pre
-              style={{
-                width: '400px',
-                margin: '10px',
-                padding: '5px',
-                border: 'black 2px solid',
-                wordBreak: 'break-all',
-                whiteSpace: 'break-spaces',
-              }}
-            >
-              {JSON.stringify(tokenData, null, 2)}
-            </pre>
-          </div>
-          <button onClick={() => logOut()}>Logout</button>
-        </>
-      ) : (
-        <>
-          <div>You are not logged in.</div>
-          <button onClick={() => login()}>Login</button>
-        </>
-      )}
-    </>
-
-      <h4>Access Token</h4>
-      <pre>{token}</pre>
-      <h4>User Information from JWT</h4>
-      <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+    {token ? (
+      <>
+        <div>
+          <h4>Access Token (JWT)</h4>
+          <pre
+            style={{
+              width: '400px',
+              margin: '10px',
+              padding: '5px',
+              border: 'black 2px solid',
+              wordBreak: 'break-all',
+              whiteSpace: 'break-spaces',
+            }}
+          >
+            {token}
+          </pre>
+        </div>
+        <button onClick={() => logOut()}>Logout</button>
+      </>
+    ) : (
+      <>
+        <div>You are not logged in.</div>
+        <button onClick={() => login()}>Login</button>
+      </>
+    )}
   </>
 }
 
