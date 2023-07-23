ff0c36e6f3956348194974f53ab95ce12029af3c
Nachiappan <nachiappann@microsoft.com>
2023-07-23 09:45:43 +0530

    Added bit bucket authentication

--- my-typescript-app/src/index.js
+++ my-typescript-app/src/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import { useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthContext, AuthProvider, TAuthConfig, TRefreshTokenExpiredEvent } from "react-oauth2-code-pkce"
+
+
+
+const authConfig = {
+  clientId: '6eb6e1f06303fb4a94296cc3452564cb01801b4e964422509497519a7fea65e4',
+  authorizationEndpoint: 'https://gitlab.com/oauth/authorize?response_type=code&grant_type=authorization_code',
+  tokenEndpoint: 'https://gitlab.com/oauth/token',
+  redirectUri: 'http://localhost:3000/',
+  scope: 'api',
+  onRefreshTokenExpire: (event) => window.confirm('Session expired. Refresh page to continue using the site?') && event.login(),
+}
+
+const UserInfo = () => {
+  const {token, tokenData} = useContext(AuthContext)
+
+  console.log(token);
+  console.log(tokenData);
+  return <>
+      <h4>Access Token</h4>
+      <pre>{token}</pre>
+      <h4>User Information from JWT</h4>
+      <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+  </>
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider authConfig={authConfig}>
+        <UserInfo/>
+    </AuthProvider>
   </React.StrictMode>
 );
 
