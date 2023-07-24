8a375f43a167fd9e8ed7c3324df987a772e7caeb
Nachiappan <nachiappann@microsoft.com>
2023-07-24 13:59:48 +0530

    Document lister

--- my-typescript-app/src/DocumentLister.js
+++ my-typescript-app/src/DocumentLister.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { useContext } from 'react';
 import './index.css';
 import { AuthContext } from "react-oauth2-code-pkce"
+import { EncryptionContext } from './EncryptionContainer';
 
 
 const DocumentLister = (props) => {
     const {token} = useContext(AuthContext);
+    const phrase = useContext(EncryptionContext);
     return <>
       {token ? (
         <>
           <div>
                 <h4>Main Document</h4>
                 <span>The token: </span>{token}
+                <div></div>
+                <span>The encryption phrase: </span>{phrase}
           </div>
         </>
       ) : (
--- my-typescript-app/src/EncryptionContainer.js
+++ my-typescript-app/src/EncryptionContainer.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { useContext } from 'react';
+import { useState, createContext, useContext } from 'react';
 import './index.css';
 import { AuthContext } from "react-oauth2-code-pkce"
 
+const EncryptionContext = createContext();
+
 const EncryptionContainer = (props) => {
+    const [encryptionPhrase, setEncryptionPhrase] = useState("Nachiappan");
     const {token} = useContext(AuthContext);
     return <>
       {token ? (
         <>
-            <div>start of encryption container</div>
-          <div>
-                {props.children}
-          </div>
-          <div>end of encryption container</div>
+            <EncryptionContext.Provider value ={encryptionPhrase}>
+                <div>start of encryption container</div>
+                <div>
+                        {props.children}
+                </div>
+                <div>end of encryption container</div>
+            </EncryptionContext.Provider>
         </>
       ) : (
         <>
@@ -22,4 +27,6 @@ const EncryptionContainer = (props) => {
     </>
   }
 
+  export { EncryptionContext }
   export default EncryptionContainer;
+
