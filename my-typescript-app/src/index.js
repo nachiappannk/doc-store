09e26d35721042c01a31d372d0843d307ed47816
Nachiappan <nachiappann@microsoft.com>
2023-07-24 23:28:09 +0530

    Updated the call back URL

--- my-typescript-app/src/index.js
+++ my-typescript-app/src/index.js
@@ -13,7 +13,7 @@ const authConfig = {
   clientId: '6eb6e1f06303fb4a94296cc3452564cb01801b4e964422509497519a7fea65e4',
   authorizationEndpoint: 'https://gitlab.com/oauth/authorize',
   tokenEndpoint: 'https://gitlab.com/oauth/token',
-  redirectUri: 'http://localhost:3000/',
+  redirectUri: 'https://wonderful-desert-0c742fe00.3.azurestaticapps.net/',
   scope: 'api read_api read_user create_runner read_repository write_repository read_registry write_registry read_observability write_observability sudo admin_mode openid profile email',
   onRefreshTokenExpire: (event) => window.confirm('Session expired. Refresh page to continue using the site?') && event.login(),
   autoLogin : false,
