3f4d602a5c450c72d86c2266cf1d8e209f24a7bd
Nachiappan <nachiappann@microsoft.com>
2023-07-24 16:53:13 +0530

    Completed the logout screen

--- my-typescript-app/src/LoginContainer.js
+++ my-typescript-app/src/LoginContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './LoginContainer.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthContext, AuthProvider, TAuthConfig, TRefreshTokenExpiredEvent } from "react-oauth2-code-pkce"
@@ -38,8 +39,11 @@ const LoginContainer = (props) => {
         </>
       ) : (
         <>
-          <div>You are not logged in.(Login container)</div>
-          <button onClick={() => login()}>Login</button>
+        <div className="login-container">
+            <p className="login-message">Kindly use your GitLab account to log in</p>
+            <p className="login-message">In case you do not possess an account, kindly proceed to create one on <a href="https://gitlab.com/" target="_blank"> Gitlab </a></p>
+            <button className="login-button" onClick={() => login()}>Login with Gitlab</button>
+        </div>
         </>
       )}
     </>
